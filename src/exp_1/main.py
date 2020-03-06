faaa28d839a87e65d3e341a661936dd80f55b4f9
Teymur Azayev <azayetey@fel.cvut.cz>
2020-03-06 12:18:42 +0100

    c

---
+++ src/exp_1/main.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
